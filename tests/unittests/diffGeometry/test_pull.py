d24e9b91b8b758bc00f816492061f06381a179ad
Max <m.spahn@tudelft.nl>
2021-07-21 16:57:04 +0200

    Add tests on pull operation

--- tests/unittests/diffGeometry/test_pull.py
+++ tests/unittests/diffGeometry/test_pull.py
@@ -57,4 +57,28 @@ def test_pullback(simple_spec, simple_differentialMap):
     assert f[0] == pytest.approx(f1[0] - f2[0])
     assert f[1] == pytest.approx(f1[1] - f2[1])
 
-
+def test_equal_results(simple_spec, simple_differentialMap):
+    s = simple_spec
+    dm = simple_differentialMap
+    dm.concretize()
+    s_pulled = s.pull(dm)
+    s_pulled.concretize()
+    s.concretize()
+    q = np.array([1.7, -np.pi/3])
+    qdot = np.array([1.2, 1.3])
+    x, J, Jdot = dm.forward(q, qdot)
+    xdot = np.dot(J, qdot)
+    Jt = np.transpose(J)
+    M_q, f_q, qddot = s_pulled.evaluate(q, qdot)
+    M_x, f_x, xddot = s.evaluate(x, xdot)
+    xddot_man = np.dot(J, qddot) + np.dot(Jdot, qdot)
+    f_q_man = np.dot(Jt, f_x) + np.dot(Jt, np.dot(M_x, np.dot(Jdot, qdot)))
+    M_q_man = np.dot(Jt, np.dot(M_x, J))
+    assert M_q[0, 0] == pytest.approx(M_q_man[0, 0])
+    assert M_q[1, 0] == pytest.approx(M_q_man[1, 0])
+    assert M_q[0, 1] == pytest.approx(M_q_man[0, 1])
+    assert M_q[1, 1] == pytest.approx(M_q_man[1, 1])
+    assert xddot_man[0] == pytest.approx(xddot[0])
+    assert xddot_man[1] == pytest.approx(xddot[1])
+    assert f_q[0] == pytest.approx(f_q_man[0])
+    assert f_q[1] == pytest.approx(f_q_man[1])
