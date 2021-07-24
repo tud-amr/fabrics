627e45b2987f5f1c78a035c2a3609a91b00b2ce2
Max <m.spahn@tudelft.nl>
2021-07-24 17:53:51 +0200

    Adds variable differential map; build up for dynamic collision avoidance

--- fabrics_package/optFabrics/diffGeometry/diffMap.py
+++ fabrics_package/optFabrics/diffGeometry/diffMap.py
@@ -30,3 +30,30 @@ class DifferentialMap:
         J = np.array(funs[1])
         Jdot = np.array(funs[2])
         return x, J, Jdot
+
+
+class VariableDifferentialMap(DifferentialMap):
+    def __init__(self, q: ca.SX, qdot: ca.SX, phi: ca.SX, q_p: ca.SX, qdot_p: ca.SX):
+        super().__init__(q, qdot, phi)
+        self._q_p = q_p
+        self._qdot_p = qdot_p
+        self._J_p = ca.jacobian(phi, q_p)
+        self._Jdot_p = Jdot_sign * ca.jacobian(ca.mtimes(self._J_p, qdot_p), q_p)
+
+    def concretize(self):
+        self._fun = ca.Function(
+            "forward", [self._q, self._qdot, self._q_p, self._qdot_p], [self._phi, self._J, self._Jdot, self._J_p, self._Jdot_p]
+        )
+
+    def forward(self, q: np.ndarray, qdot: np.ndarray, q_p: np.ndarray, qdot_p: np.ndarray):
+        assert isinstance(q, np.ndarray)
+        assert isinstance(qdot, np.ndarray)
+        assert isinstance(q_p, np.ndarray)
+        assert isinstance(qdot_p, np.ndarray)
+        funs = self._fun(q, qdot, q_p, qdot_p)
+        x = np.array(funs[0])[:, 0]
+        J = np.array(funs[1])
+        Jdot = np.array(funs[2])
+        J_p = np.array(funs[3])
+        Jdot_p = np.array(funs[4])
+        return x, J, Jdot, J_p, Jdot_p
--- tests/unittests/diffGeometry/test_differentialMap.py
+++ tests/unittests/diffGeometry/test_differentialMap.py
@@ -1,7 +1,7 @@
 import pytest
 import casadi as ca
 import numpy as np
-from optFabrics.diffGeometry.diffMap import DifferentialMap
+from optFabrics.diffGeometry.diffMap import DifferentialMap, VariableDifferentialMap
 from optFabrics.diffGeometry.variables import Jdot_sign
 
 
@@ -20,6 +20,19 @@ def simple_differentialMap():
     phi = ca.vertcat(q[0] * ca.cos(q[1]), q[0] * ca.sin(q[1]))
     dm = DifferentialMap(q, qdot, phi)
     return dm
+
+
+@pytest.fixture
+def variable_differentialMap():
+    q = ca.SX.sym("q", 2)
+    qdot = ca.SX.sym("qdot", 2)
+    q_p = ca.SX.sym("q_p", 2)
+    qdot_p = ca.SX.sym("qdot_p", 2)
+    phi = ca.norm_2(q - np.zeros(2))
+    phi_var = ca.norm_2(q - q_p)
+    dm = DifferentialMap(q, qdot, phi)
+    dm_var = VariableDifferentialMap(q, qdot, phi_var, q_p, qdot_p)
+    return dm, dm_var
 
 
 def test_forward_mapping_polar(simple_differentialMap):
@@ -55,3 +68,65 @@ def test_forward_mapping_polar(simple_differentialMap):
     assert Jdot[0, 1] == pytest.approx(Jdot_test[0, 1])
     assert Jdot[1, 0] == pytest.approx(Jdot_test[1, 0])
     assert Jdot[1, 1] == pytest.approx(Jdot_test[1, 1])
+
+
+def test_variable_map_Zero(variable_differentialMap):
+    dm = variable_differentialMap[0]
+    dm_var = variable_differentialMap[1]
+    dm.concretize()
+    dm_var.concretize()
+    q = np.array([1.0, -0.2])
+    qdot = np.array([-0.3, 0.7])
+    q_p = np.array([0.0, 0.0])
+    qdot_p = np.array([0.0, 0.0])
+    x, J, Jdot = dm.forward(q, qdot)
+    x_var, J_var, Jdot_var, J_p, Jdot_p = dm_var.forward(q, qdot, q_p, qdot_p)
+    xdot = np.dot(J, qdot)
+    xdot_var = np.dot(J_var, qdot)
+    assert x[0] == pytest.approx(np.linalg.norm(q - q_p))
+    assert x_var[0] == pytest.approx(np.linalg.norm(q - q_p))
+    assert J[0, 0] == pytest.approx(J_var[0, 0])
+    assert Jdot[0, 1] == pytest.approx(Jdot_var[0, 1])
+    assert Jdot[0, 0] == pytest.approx(Jdot_var[0, 0])
+    assert J[0, 1] == pytest.approx(J_var[0, 1])
+    xdot_test = 1 / np.linalg.norm(q) * (q[0] * qdot[0] + q[1] * qdot[1])
+    assert xdot[0] == pytest.approx(xdot_test)
+    assert xdot_var[0] == pytest.approx(xdot_test)
+
+
+def test_variable_map_NonZero(variable_differentialMap):
+    dm_var = variable_differentialMap[1]
+    dm_var.concretize()
+    q = np.array([1.0, -0.2])
+    qdot = np.array([-0.3, 0.7])
+    q_p = np.array([-0.2, 0.2])
+    qdot_p = np.array([-0.4, 0.8])
+    x, J, Jdot, J_p, Jdot_p = dm_var.forward(q, qdot, q_p, qdot_p)
+    xdot = np.dot(J, qdot) + np.dot(J_p, qdot_p)
+    J_test = 1 / np.linalg.norm(q - q_p) * np.array([q[0] - q_p[0], q[1] - q_p[1]])
+    Jdot_test = Jdot_sign * (
+        1 / np.linalg.norm(q - q_p) * qdot
+        - 1 / (np.linalg.norm(q - q_p) ** 3) * np.dot(q - q_p, qdot) * (q - q_p)
+    )
+    Jdot_p_test = Jdot_sign * (
+        + 1 / np.linalg.norm(q - q_p) * qdot_p
+        - 1 / (np.linalg.norm(q - q_p) ** 3) * np.dot(q - q_p, qdot_p) * (q - q_p)
+    )
+    assert x[0] == pytest.approx(np.linalg.norm(q - q_p))
+    assert J[0, 1] == pytest.approx(J_test[1])
+    assert J[0, 0] == pytest.approx(J_test[0])
+    assert Jdot[0, 1] == pytest.approx(Jdot_test[1])
+    assert Jdot[0, 0] == pytest.approx(Jdot_test[0])
+    assert Jdot_p[0, 1] == pytest.approx(Jdot_p_test[1])
+    assert Jdot_p[0, 0] == pytest.approx(Jdot_p_test[0])
+    xdot_p_test1 = (
+        1
+        / np.linalg.norm(q - q_p)
+        * ((q[0] - q_p[0]) * qdot[0] + (q[1] - q_p[1]) * qdot[1])
+    )
+    xdot_p_test2 = (
+        -1
+        / np.linalg.norm(q - q_p)
+        * ((q[0] - q_p[0]) * qdot_p[0] + (q[1] - q_p[1]) * qdot_p[1])
+    )
+    assert xdot[0] == pytest.approx(xdot_p_test1 + xdot_p_test2)
