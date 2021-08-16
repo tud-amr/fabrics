20a67ae4046996f658fc63552911e5228e69314e
Max <m.spahn@tudelft.nl>
2021-08-16 16:57:49 +0200

    Adds dynamic euler lagrange equation to replace pulling specs when pulling energies

--- fabrics_package/optFabrics/diffGeometry/energy.py
+++ fabrics_package/optFabrics/diffGeometry/energy.py
@@ -24,14 +24,12 @@ class Lagrangian(object):
         assert isinstance(l, ca.SX)
         if 'x' in kwargs:
             self._vars = [kwargs.get('x'), kwargs.get('xdot')]
-            self.applyEulerLagrange()
+            if 'x_p' in kwargs:
+                self._vars += [kwargs.get('x_p'), kwargs.get('xdot_p'), kwargs.get('xddot_p')]
         elif 'var' in kwargs:
             self._vars = kwargs.get('var')
-            self.applyEulerLagrange()
-        if 's' in kwargs:
-            s = kwargs.get('s')
-            self._vars = s._vars
-            self._S = s
+        self._rel = True if len(self._vars) > 2 else False
+        self.applyEulerLagrange()
 
     def x(self):
         return self._vars[0]
@@ -39,26 +37,56 @@ class Lagrangian(object):
     def xdot(self):
         return self._vars[1]
 
+    def x_p(self):
+        if self._rel:
+            return self._vars[2]
+        else:
+            return None
+
+    def xdot_p(self):
+        if self._rel:
+            return self._vars[3]
+        else:
+            return None
+
+    def xddot_p(self):
+        if self._rel:
+            return self._vars[4]
+        else:
+            return None
+
     def __add__(self, b):
         assert isinstance(b, Lagrangian)
         checkCompatability(self, b)
-        return Lagrangian(self._l + b._l, s=self._S + b._S)
+        return Lagrangian(self._l + b._l, var=joinVariables(self._vars, b._vars))
 
     def applyEulerLagrange(self):
-        dL_dx = ca.gradient(self._l, self.x())
-        dL_dxdot = ca.gradient(self._l, self.xdot())
-        d2L_dxdxdot = ca.jacobian(dL_dx, self.xdot())
-        d2L_dxdot2 = ca.jacobian(dL_dxdot, self.xdot())
+        dL_dxdot = ca.gradient(self._l, self._vars[1])
+        dL_dx = ca.gradient(self._l, self._vars[0])
+        d2L_dxdxdot = ca.jacobian(dL_dx, self._vars[1])
+        d2L_dxdot2 = ca.jacobian(dL_dxdot, self._vars[1])
+        f_rel = np.zeros(self.x().size()[0])
+        en_rel = np.zeros(1)
+
+        if self._rel:
+            dL_dxpdot = ca.gradient(self._l, self.xdot_p())
+            d2L_dxdotdxpdot = ca.jacobian(dL_dxdot, self.xdot_p())
+            d2L_dxdotdxp = ca.jacobian(dL_dxdot, self.x_p())
+            f_rel1 = ca.mtimes(d2L_dxdotdxpdot, self.xddot_p())
+            f_rel2 = ca.mtimes(d2L_dxdotdxp, self.xdot_p())
+            f_rel = f_rel1 + f_rel2
+            en_rel = ca.dot(dL_dxpdot, self.xdot_p())
 
         F = d2L_dxdxdot
         f_e = -dL_dx
         M = d2L_dxdot2
-        f = ca.mtimes(ca.transpose(F), self.xdot()) + f_e
+        f = ca.mtimes(ca.transpose(F), self._vars[1]) + f_e + f_rel
+        self._H = ca.dot(dL_dxdot, self._vars[1]) - self._l + en_rel
         self._S = Spec(M, f=f, var=self._vars)
 
     def concretize(self):
         self._S.concretize()
-        self._l_fun = ca.Function("funs", self._vars, [self._l])
+        self._l_fun = ca.Function("funs", self._vars, [self._H])
 
     def evaluate(self, *args):
         for arg in args:
@@ -70,26 +98,9 @@ class Lagrangian(object):
     def pull(self, dm: DifferentialMap):
         assert isinstance(dm, DifferentialMap)
         l_subst = ca.substitute(self._l, self._vars[0], dm._phi)
-        l_pulled = ca.substitute(l_subst, self._vars[1], dm.phidot())
-        s_pulled = self._S.pull(dm)
-        if isinstance(dm, RelativeDifferentialMap):
-            return RelativeLagrangian(l_pulled, dm=dm, s=s_pulled)
-        else:
-            return Lagrangian(l_pulled, s=s_pulled)
-
-
-class RelativeLagrangian(Lagrangian):
-
-    def __init__(self, l: ca.SX, **kwargs):
-        self._dm = kwargs.get('dm')
-        super().__init__(l, **kwargs)
-        self._vars = joinVariables(self._dm._vars, self._vars)
-
-    def x(self):
-        return self._vars[0]
-
-    def xdot(self):
-        return (self._vars[1] - self._dm.qdot_p())
+        l_subst2 = ca.substitute(l_subst, self._vars[1], dm.phidot())
+        new_vars = joinVariables(self._vars[2:], dm._vars)
+        return Lagrangian(l_subst2, var=new_vars)
 
 
 class FinslerStructure(Lagrangian):
