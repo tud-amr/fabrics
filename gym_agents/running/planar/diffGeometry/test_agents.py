0810607b0b94912f43e3630549e9bdaecf346935
Max <m.spahn@tudelft.nl>
2021-12-03 17:48:39 +0100

    Removes not updated point_mass agent from test_agents

--- gym_agents/running/planar/diffGeometry/test_agents.py
+++ gym_agents/running/planar/diffGeometry/test_agents.py
@@ -1,7 +1,7 @@
 import pytest
 
 from col_avoidance import pointMassAvoidance
-from pointMass import pointMass
+#from pointMass import pointMass
 from pointMass_dynamic import pointMassDynamicGoal
 from nlink import nlink
 from nlink_dynamic import nlinkDynamicGoal
@@ -11,10 +11,11 @@ def test_pointMassAvoidance():
     res = pointMassAvoidance(10)
     assert res is None
 
-
+"""
 def test_pointMass():
     res = pointMass(10)
     assert res
+"""
 
 
 def test_pointMassDynamic():
