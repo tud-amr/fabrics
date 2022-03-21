ecddf9808e3b5e4014fa5585e79dc4f61f9c649d
Max Spahn <m.spahn@tudelft.nl>
2022-03-21 10:26:06 +0100

    Updates to new forward kinematics for tiago.

--- gym_agents/tiago/tiagoAgent.py
+++ gym_agents/tiago/tiagoAgent.py
@@ -1,4 +1,5 @@
 import gym
+
 # import nLinkUrdfReacher
 import time
 import os
@@ -7,7 +8,11 @@ import numpy as np
 import casadi as ca
 
 from fabrics.planner.nonHolonomicPlanner import DefaultNonHolonomicPlanner
-from fabrics.planner.default_geometries import CollisionGeometry, LimitGeometry, GoalGeometry
+from fabrics.planner.default_geometries import (
+    CollisionGeometry,
+    LimitGeometry,
+    GoalGeometry,
+)
 from fabrics.planner.default_energies import CollisionLagrangian, ExecutionLagrangian
 from fabrics.planner.default_maps import CollisionMap, UpperLimitMap, LowerLimitMap
 from fabrics.planner.default_leaves import defaultAttractor
@@ -22,11 +27,15 @@ from forwardkinematics.urdfFks.tiagoFk import TiagoFk
 def tiagoFabric(n_steps=1000, render=True):
     ## setting up the problem
     n = 20
-    obst1Dict = {'dim': 3, 'type': 'sphere', 'geometry': {'position': [0.5, 0.0, 0.0], 'radius': 0.2}} 
+    obst1Dict = {
+        "dim": 3,
+        "type": "sphere",
+        "geometry": {"position": [0.5, 0.0, 0.0], "radius": 0.2},
+    }
     obsts = [
         SphereObstacle(name="obst1", contentDict=obst1Dict),
     ]
-    planner = DefaultNonHolonomicPlanner(n, m_base=1, m_arm=0.1)
+    planner = DefaultNonHolonomicPlanner(n, m_base=1, m_arm=0.01)
     q, qdot = planner.var()
     tiagoFk = TiagoFk()
     # collision avoidance
@@ -34,23 +43,68 @@ def tiagoFabric(n_steps=1000, render=True):
     xdot = ca.SX.sym("xdot", 1)
     lag_col = CollisionLagrangian(x, xdot)
     geo_col = CollisionGeometry(x, xdot, exp=3, lam=1)
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         fk = tiagoFk.fk(q, i, positionOnly=True)
         for obst in obsts:
             dm_col = CollisionMap(q, qdot, fk, obst.position(), obst.radius())
-            #planner.addGeometry(dm_col, lag_col, geo_col)
+            # planner.addGeometry(dm_col, lag_col, geo_col)
 
     # joint limits
     x = ca.SX.sym("x", 1)
     xdot = ca.SX.sym("xdot", 1)
     lag_col = CollisionLagrangian(x, xdot)
-    geo_col = LimitGeometry(
-        x, xdot, lam=0.5, exp=1
-    )
-    dm_col = UpperLimitMap(q, qdot, 0.2, 3)
-    planner.addGeometry(dm_col, lag_col, geo_col)
-    dm_col = LowerLimitMap(q, qdot, -0.1, 3)
-    planner.addGeometry(dm_col, lag_col, geo_col)
+    geo_col = LimitGeometry(x, xdot, lam=0.5, exp=2)
+    limits = [
+        [
+            -10.0,
+            -10.0,
+            -6.28318531,
+            0.0,
+            -1.30899694,
+            -1.04719755,
+            -1.17809725,
+            -1.17809725,
+            -0.78539816,
+            -0.39269908,
+            -2.0943951,
+            -1.41371669,
+            -2.0943951,
+            -1.17809725,
+            -1.17809725,
+            -0.78539816,
+            -0.39269908,
+            -2.0943951,
+            -1.41371669,
+            -2.0943951,
+        ],
+        [
+            10.0,
+            10.0,
+            6.28318531,
+            0.35,
+            1.30899694,
+            0.78539816,
+            1.57079633,
+            1.57079633,
+            3.92699082,
+            2.35619449,
+            2.0943951,
+            1.41371669,
+            2.0943951,
+            1.57079633,
+            1.57079633,
+            3.92699082,
+            2.35619449,
+            2.0943951,
+            1.41371669,
+            2.0943951,
+        ],
+    ]
+    for i in range(3, n):
+        dm_col = UpperLimitMap(q, qdot,limits[1][i], i)
+        planner.addGeometry(dm_col, lag_col, geo_col)
+        dm_col = LowerLimitMap(q, qdot, limits[0][i], i)
+        planner.addGeometry(dm_col, lag_col, geo_col)
     goal1Dict = {
         "m": 3,
         "w": 1.0,
@@ -58,15 +112,16 @@ def tiagoFabric(n_steps=1000, render=True):
         "indices": [0, 1, 2],
         "parent_link": 0,
         "child_link": 11,
-        "desired_position": [-0.6, 0.8, 0.8],
+        "desired_position": [-0.1, 0.8, 0.8],
         "epsilon": 0.05,
         "type": "staticSubGoal",
     }
-    goal1 = StaticSubGoal(name='goal', contentDict=goal1Dict)
-    fk_ee = tiagoFk.fk(q, goal1.childLink(), positionOnly=True)
-    dm_psi, lag_psi, geo_psi, x_psi, xdot_psi = defaultAttractor(q, qdot, goal1.position(), fk_ee)
-    geo_psi = GoalGeometry(x_psi, xdot_psi, k_psi=5)
-    planner.addForcingGeometry(dm_psi, lag_psi, geo_psi)
+    goal1 = StaticSubGoal(name="goal1", contentDict=goal1Dict)
+    fk_ee1 = tiagoFk.fk(q, goal1.childLink(), positionOnly=True)
+    dm_psi1, lag_psi1, _, x_psi1, xdot_psi1 = defaultAttractor(
+        q, qdot, goal1.position(), fk_ee1
+    )
+    geo_psi1 = GoalGeometry(x_psi1, xdot_psi1, k_psi=10)
     goal2Dict = {
         "m": 3,
         "w": 1.0,
@@ -78,37 +133,42 @@ def tiagoFabric(n_steps=1000, render=True):
         "epsilon": 0.05,
         "type": "staticSubGoal",
     }
-    goal2 = StaticSubGoal(name='goal', contentDict=goal2Dict)
-    fk_ee = tiagoFk.fk(q, goal2.childLink(), positionOnly=True)
-    dm_psi, lag_psi, geo_psi, x_psi, xdot_psi = defaultAttractor(q, qdot, goal2.position(), fk_ee)
-    geo_psi = GoalGeometry(x_psi, xdot_psi, k_psi=5)
-    planner.addForcingGeometry(dm_psi, lag_psi, geo_psi)
+    goal2 = StaticSubGoal(name="goal2", contentDict=goal2Dict)
+    fk_ee2 = tiagoFk.fk(q, goal2.childLink(), positionOnly=True)
+    dm_psi2, lag_psi2, _, x_psi2, xdot_psi2 = defaultAttractor(
+        q, qdot, goal2.position(), fk_ee2
+    )
+    geo_psi2 = GoalGeometry(x_psi2, xdot_psi2, k_psi=5)
+    # adding forcing terms
+    planner.addForcingGeometry(dm_psi1, lag_psi1, geo_psi1)
+    planner.addForcingGeometry(dm_psi2, lag_psi2, geo_psi2)
     # execution energy
     exLag = ExecutionLagrangian(q, qdot)
     planner.setExecutionEnergy(exLag)
-    # speed control 
+    # speed control
     ex_factor = 1.0
-    planner.setDefaultSpeedControl(x_psi, dm_psi, exLag, ex_factor, r_b=0.2)
+    planner.setDefaultSpeedControl(x_psi1, dm_psi1, exLag, ex_factor, r_b=0.2)
     planner.concretize()
     ## running the simulation
-    env = gym.make('tiago-reacher-acc-v0', dt=0.01, render=render)
+    env = gym.make("tiago-reacher-acc-v0", dt=0.01, render=render)
     print("Starting episode")
-    #env.add_obstacle(obsts[0])
+    # env.add_obstacle(obsts[0])
     env.add_goal(goal1)
     env.add_goal(goal2)
     q0 = np.zeros(20)
+    q0[3] = 0.1
     q0[6] = 1
-    q0[7] = 1
+    q0[7] = 0
+    q0[8] = 2
     q0[9] = 1
     q0[13] = 1
     q0[14] = 1
     q0[16] = 1
     ob = env.reset(pos=q0)
     for i in range(n_steps):
-        qudot = np.concatenate((ob['vel'], ob['xdot'][3:]))
-        action = planner.computeAction(ob['x'], ob['xdot'], qudot)
-        print(f"tiagoFk.fk(ob['x'], goal1.childLink(), positionOnly=True) : {tiagoFk.fk(ob['x'], goal1.childLink(), positionOnly=True)}")
-        ob, reward, done, info = env.step(action)
+        qudot = np.concatenate((ob["vel"], ob["xdot"][3:]))
+        action = planner.computeAction(ob["x"], ob["xdot"], qudot)
+        ob, _, _, _ = env.step(action)
     return {}
 
 
