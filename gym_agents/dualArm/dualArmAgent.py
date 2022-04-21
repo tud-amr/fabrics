ce65aa0770b2142b7a237876a3c2277aa32ab30f
Max Spahn <m.spahn@tudelft.nl>
2022-04-21 10:55:10 +0200

    Solves python dataclasses bug and fixes changes to dualArm examples.

--- gym_agents/dualArm/dualArmAgent.py
+++ gym_agents/dualArm/dualArmAgent.py
@@ -6,10 +6,10 @@ import numpy as np
 import casadi as ca
 
 from fabrics.planner.fabricPlanner import DefaultFabricPlanner
-from fabrics.planner.default_geometries import CollisionGeometry, LimitGeometry, GoalGeometry
-from fabrics.planner.default_energies import CollisionLagrangian, ExecutionLagrangian
-from fabrics.planner.default_maps import CollisionMap, UpperLimitMap, LowerLimitMap
-from fabrics.planner.default_leaves import defaultAttractor
+from fabrics.defaults.default_geometries import CollisionGeometry, LimitGeometry, GoalGeometry
+from fabrics.defaults.default_energies import CollisionLagrangian, ExecutionLagrangian
+from fabrics.defaults.default_maps import CollisionMap, UpperLimitMap, LowerLimitMap
+from fabrics.defaults.default_leaves import defaultAttractor
 
 from fabrics.helpers.variables import Variables
 
