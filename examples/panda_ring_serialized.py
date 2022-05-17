90d5ae359df1706f4a3e8db25f6028ee1a50e288
Max Spahn <m.spahn@tudelft.nl>
2022-05-17 17:14:23 +0200

    Cleanup serialized example.

--- examples/panda_ring_serialized.py
+++ examples/panda_ring_serialized.py
@@ -1,26 +1,16 @@
 import gym
-import urdfenvs.panda_reacher  #pylint: disable=unused-import
+import urdfenvs.panda_reacher  # pylint: disable=unused-import
 
 from MotionPlanningGoal.goalComposition import GoalComposition
 from MotionPlanningEnv.sphereObstacle import SphereObstacle
-from forwardkinematics.urdfFks.pandaFk import PandaFk
 
 import numpy as np
 from fabrics.planner.serialized_planner import SerializedFabricPlanner
 
 import os
-import casadi as ca
-import pickle
 
 
-# serialized file name
-# 10 denotes 10 spheres
-serialized_file = "serialized_10.pkl"
-
-# boolean to determine if the serialized file has been loaded
-isload = False
-
-def initalize_environment(render=True, obstacle_resolution = 8):
+def initalize_environment(render=True, obstacle_resolution=8):
     """
     Initializes the simulation environment.
 
@@ -35,7 +25,7 @@ def initalize_environment(render=True, obstacle_resolution = 8):
     obstacles = []
     whole_position = [0.4, 0.0, 0.7]
     for i in range(obstacle_resolution + 1):
-        angle = i/obstacle_resolution * 2.*np.pi
+        angle = i / obstacle_resolution * 2.0 * np.pi
         position = [
             whole_position[0],
             whole_position[1] + radius_ring * np.cos(angle),
@@ -46,7 +36,9 @@ def initalize_environment(render=True, obstacle_resolution = 8):
             "type": "sphere",
             "geometry": {"position": position, "radius": 0.05},
         }
-        obstacles.append(SphereObstacle(name="staticObst", contentDict=static_obst_dict))
+        obstacles.append(
+            SphereObstacle(name="staticObst", contentDict=static_obst_dict)
+        )
     # Definition of the goal.
     goal_position = whole_position
     goal_position[0] += 0.3
@@ -72,7 +64,7 @@ def initalize_environment(render=True, obstacle_resolution = 8):
             "desired_position": [0.0, 0.0],
             "epsilon": 0.05,
             "type": "staticSubGoal",
-        }
+        },
     }
     goal = GoalComposition(name="goal", contentDict=goal_dict)
     env.add_goal(goal)
@@ -81,49 +73,20 @@ def initalize_environment(render=True, obstacle_resolution = 8):
     return (env, obstacles, goal, initial_observation)
 
 
-def set_planner(goal: GoalComposition, degrees_of_freedom: int = 7, obstacle_resolution = 10):
+def set_planner(file_name):
     """
-    Initializes the fabric planner for the panda robot.
-
-    This function defines the forward kinematics for collision avoidance,
-    and goal reaching. These components are fed into the fabrics planner.
-
-    In the top section of this function, an example for optional reconfiguration
-    can be found. Commented by default.
+    Initializes the fabric planner for the panda robot from the file.
 
     Params
-    ----------
-    goal: StaticSubGoal
-        The goal to the motion planning problem.
-    degrees_of_freedom: int
-        Degrees of freedom of the robot (default = 7)
+    ---------
+    file_name: str
+        File name to which the planner has been serialized.
+
     """
 
-    robot_type = 'panda'
-
-
-    attractor_potential = "10 * ca.norm_2(x)**2"
-    collision_finsler = "2.0/(x ** 2) * xdot ** 2"
     planner = SerializedFabricPlanner(
-        serialized_file,
+        file_name,
     )
-
-    if not planner._isload:
-        print(f"Serializing planner function and input_keys to {serialized_file}")
-        q = planner.variables.position_variable()
-        panda_fk = PandaFk()
-        forward_kinematics = []
-        for i in range(1, degrees_of_freedom+1):
-            forward_kinematics.append(panda_fk.fk(q, i, positionOnly=True))
-        # The planner hides all the logic behind the function set_components.
-        planner.set_components(
-            forward_kinematics,
-            goal,
-            number_obstacles=obstacle_resolution,
-        )
-        # concertize here
-        planner.serialize(serialized_file)
-
     return planner
 
 
@@ -132,52 +95,35 @@ def run_panda_ring_serialized_example(n_steps=5000, render=True):
     (env, obstacles, goal, initial_observation) = initalize_environment(
         render=render, obstacle_resolution=obstacle_resolution_ring
     )
+    file_name = "serialized_10.pkl"
     ob = initial_observation
-    planner = set_planner(goal, obstacle_resolution= obstacle_resolution_ring)
+    planner = set_planner(file_name)
 
     # Start the simulation
     print("Starting simulation")
     sub_goal_0_position = np.array(goal.subGoals()[0].position())
-    sub_goal_0_weight= np.array(goal.subGoals()[0].weight())
+    sub_goal_0_weight = np.array(goal.subGoals()[0].weight())
     sub_goal_1_position = np.array(goal.subGoals()[1].position())
-    sub_goal_1_weight= np.array(goal.subGoals()[1].weight())
+    sub_goal_1_weight = np.array(goal.subGoals()[1].weight())
     obstacle_positions = []
     obstacle_radii = []
     for obst in obstacles:
         obstacle_positions.append(obst.position())
         obstacle_radii.append(np.array(obst.radius()))
 
-    # if serialized_file exists and is loaded
-    # pass loaded data [planner._funs, planner._input_keys] to serialized_compute_action
-    # otherwise use the compute_action() from ParameterizedFabricPlanner
-    if os.path.isfile(serialized_file) and planner._isload:
-        for _ in range(n_steps):
-            action = planner.compute_action(
-                q=ob["x"],
-                qdot=ob["xdot"],
-                x_goal_0=sub_goal_0_position,
-                weight_goal_0=sub_goal_0_weight,
-                x_goal_1=sub_goal_1_position,
-                weight_goal_1=sub_goal_1_weight,
-                x_obsts = obstacle_positions,
-                radius_obsts = obstacle_radii,
-                radius_body=np.array([0.02]),
-            )
-            ob, *_ = env.step(action)
-    else:
-        for _ in range(n_steps):
-            action = planner.compute_action(
-                q=ob["x"],
-                qdot=ob["xdot"],
-                x_goal_0=sub_goal_0_position,
-                weight_goal_0=sub_goal_0_weight,
-                x_goal_1=sub_goal_1_position,
-                weight_goal_1=sub_goal_1_weight,
-                x_obsts = obstacle_positions,
-                radius_obsts = obstacle_radii,
-                radius_body=np.array([0.02]),
-            )
-            ob, *_ = env.step(action)
+    for _ in range(n_steps):
+        action = planner.compute_action(
+            q=ob["x"],
+            qdot=ob["xdot"],
+            x_goal_0=sub_goal_0_position,
+            weight_goal_0=sub_goal_0_weight,
+            x_goal_1=sub_goal_1_position,
+            weight_goal_1=sub_goal_1_weight,
+            x_obsts=obstacle_positions,
+            radius_obsts=obstacle_radii,
+            radius_body=np.array([0.02]),
+        )
+        ob, *_ = env.step(action)
     return {}
 
 
