0629128cd7194990abd084ceeda9228189d7e3fa
Max Spahn <m.spahn@tudelft.nl>
2022-05-23 15:29:26 +0200

    Adds panda example with limits.

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,4 +1,5 @@
 from planar_arm import run_planar_arm_example
+from planar_arm_limits import run_planar_arm_limits_example
 from planar_point_robot import run_point_robot_example
 from planar_point_robot_line_goal import run_point_robot_line_goal_example
 from panda import run_panda_example
@@ -6,6 +7,7 @@ from panda_orientation import run_panda_orientation_example
 from panda_trajectory_following import run_panda_trajectory_example
 from panda_ring import run_panda_ring_example
 from panda_ring_serialized import run_panda_ring_serialized_example
+from panda_limits import run_panda_limits
 
 def test_panda_example():
     res = run_panda_example(10, render=False)
@@ -18,10 +20,18 @@ def test_panda_ring_example():
 def test_panda_trajectory():
     res = run_panda_trajectory_example(10, render=False)
     assert isinstance(res, dict)
+
+def test_panda_limits():
+    res = run_panda_limits(10, render=False)
+    assert isinstance(res, dict)
     
 
 def test_planar_arm_example():
     res = run_planar_arm_example(10, render=False)
+    assert isinstance(res, dict)
+
+def test_planar_arm_limits_example():
+    res = run_planar_arm_limits_example(10, render=False)
     assert isinstance(res, dict)
 
 def test_planar_point_robot_example():
