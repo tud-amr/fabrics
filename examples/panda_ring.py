Fixes orientation error with rotation matrix. It needed a transpose. (#32)

examples/panda_ring.py
@@ -10,8 +10,8 @@ from MotionPlanningGoal.goalComposition import GoalComposition
 from MotionPlanningEnv.sphereObstacle import SphereObstacle
 
 import numpy as np
+import quaternionic
 from fabrics.planner.parameterized_planner import ParameterizedFabricPlanner
-
 
 def initalize_environment(render=True, obstacle_resolution = 8):
     """
@@ -24,25 +24,30 @@ def initalize_environment(render=True, obstacle_resolution = 8):
     q0 = np.array([0.0, -1.0, 0.0, -1.501, 0.0, 1.8675, 0.0])
     initial_observation = env.reset(pos=q0)
     # Definition of the obstacle.
-    radius_ring = 0.25
+    radius_ring = 0.3
     obstacles = []
-    whole_position = [0.7, 0.2, 0.9]
+    goal_orientation = [-0.366, 0.0, 0.0, 0.9305]
+    goal_orientation = [0.28888, 0.65109, 0.9165, 0.49848]
+    #goal_orientation = np.random.random(4).tolist()
+    print(goal_orientation)
+    rotation_matrix = quaternionic.array(goal_orientation).to_rotation_matrix
+    whole_position = [0.1, 0.6, 0.8]
     for i in range(obstacle_resolution + 1):
         angle = i/obstacle_resolution * 2.*np.pi
-        position = [
-            whole_position[0],
-            whole_position[1] + radius_ring * np.cos(angle),
-            whole_position[2] + radius_ring * np.sin(angle),
+        origin_position = [
+            0.0,
+            radius_ring * np.cos(angle),
+            radius_ring * np.sin(angle),
         ]
+        position = np.dot(rotation_matrix, origin_position) + whole_position
         static_obst_dict = {
             "dim": 3,
             "type": "sphere",
-            "geometry": {"position": position, "radius": 0.15},
+            "geometry": {"position": position, "radius": 0.1},
         }
         obstacles.append(SphereObstacle(name="staticObst", contentDict=static_obst_dict))
     # Definition of the goal.
     goal_position = whole_position
-    goal_position[0] += 0.1
     goal_dict = {
         "subgoal0": {
             "m": 3,
@@ -50,19 +55,20 @@ def initalize_environment(render=True, obstacle_resolution = 8):
             "prime": True,
             "indices": [0, 1, 2],
             "parent_link": "panda_link0",
-            "child_link": "panda_vacuum",
+            "child_link": "panda_hand",
             "desired_position": whole_position,
             "epsilon": 0.05,
             "type": "staticSubGoal",
         },
         "subgoal1": {
-            "m": 2,
-            "w": 5.0,
+            "m": 3,
+            "w": 3.0,
             "prime": False,
-            "indices": [1, 2],
+            "indices": [0, 1, 2],
             "parent_link": "panda_link7",
             "child_link": "panda_hand",
-            "desired_position": [0.0, 0.0],
+            "desired_position": [0.107, 0.0, 0.0],
+            "angle": goal_orientation,
             "epsilon": 0.05,
             "type": "staticSubGoal",
         }
@@ -96,7 +102,7 @@ def set_planner(goal: GoalComposition, degrees_of_freedom: int = 7, obstacle_res
 
     ## Optional reconfiguration of the planner
     # base_inertia = 0.03
-    attractor_potential = "5.0 * (ca.norm_2(x) + 1 /10 * ca.log(1 + ca.exp(-2 * 10 * ca.norm_2(x))))"
+    # attractor_potential = "5.0 * (ca.norm_2(x) + 1 /10 * ca.log(1 + ca.exp(-2 * 10 * ca.norm_2(x))))"
     # damper = {
     #     "alpha_b": 0.5,
     #     "alpha_eta": 0.5,
@@ -115,14 +121,6 @@ def set_planner(goal: GoalComposition, degrees_of_freedom: int = 7, obstacle_res
     # attractor_potential = "15.0 * (ca.norm_2(x) + 1 /10 * ca.log(1 + ca.exp(-2 * 10 * ca.norm_2(x))))"
     # collision_geometry= "-0.1 / (x ** 2) * (-0.5 * (ca.sign(xdot) - 1)) * xdot ** 2"
     # collision_finsler= "0.1/(x**1) * xdot**2"
-    damper = {
-        "alpha_b": 0.5,
-        "alpha_eta": 0.5,
-        "alpha_shift": 0.5,
-        "beta_distant": 0.01,
-        "beta_close": 6.5,
-        "radius_shift": 0.1,
-    }
     absolute_path = os.path.dirname(os.path.abspath(__file__))
     with open(absolute_path + "/albert_polluted_2.urdf", "r") as file:
         urdf = file.read()
@@ -137,7 +135,7 @@ def set_planner(goal: GoalComposition, degrees_of_freedom: int = 7, obstacle_res
             [2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973],
             [-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973],
         ]
-    collision_links = ['panda_link8', 'panda_link4', 'panda_vacuum', "panda_vacuum_2"]
+    collision_links = ['panda_link1', 'panda_link4', 'panda_link6', 'panda_hand']
     self_collision_pairs = {}
     # The planner hides all the logic behind the function set_components.
     planner.set_components(
@@ -152,7 +150,7 @@ def set_planner(goal: GoalComposition, degrees_of_freedom: int = 7, obstacle_res
 
 
 def run_panda_ring_example(n_steps=5000, render=True, serialize=False):
-    obstacle_resolution_ring = 6
+    obstacle_resolution_ring = 10
     (env, obstacles, goal, initial_observation) = initalize_environment(
         render=render, obstacle_resolution=obstacle_resolution_ring
     )
@@ -175,11 +173,10 @@ def run_panda_ring_example(n_steps=5000, render=True, serialize=False):
         obstacle_positions.append(obst.position())
         obstacle_radii.append(np.array(obst.radius()))
 
+    sub_goal_0_quaternion = quaternionic.array(goal.subGoals()[1].angle())
+    sub_goal_0_rotation_matrix = sub_goal_0_quaternion.to_rotation_matrix
+
     for i in range(n_steps):
-        if i % 100 == 0:
-            print(i)
-        if i == 500:
-            sub_goal_0_position = np.array([0.4, -0.7, 0.3])
 
         action = planner.compute_action(
             q=ob["x"],
@@ -190,12 +187,14 @@ def run_panda_ring_example(n_steps=5000, render=True, serialize=False):
             weight_goal_1=sub_goal_1_weight,
             x_obsts = obstacle_positions,
             radius_obsts = obstacle_radii,
+            radius_body_panda_link1=np.array([0.1]),
             radius_body_panda_link4=np.array([0.1]),
-            radius_body_panda_link8=np.array([0.1]),
-            radius_body_panda_vacuum=np.array([0.03]),
-            radius_body_panda_vacuum_2=np.array([0.03]),
+            radius_body_panda_link6=np.array([0.15]),
+            radius_body_panda_hand=np.array([0.1]),
+            angle_goal_1=sub_goal_0_rotation_matrix,
         )
         ob, *_ = env.step(action)
+
     return {}
 
 