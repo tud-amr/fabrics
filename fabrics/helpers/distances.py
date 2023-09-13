99f8a803b803d60b8f7ca2be8ba033f1e6b5b53d
Max Spahn <m.spahn@tudelft.nl>
2023-09-13 13:54:15 +0200

    ft[distances]: Adds sample-free distance computation for cuboids.

--- fabrics/helpers/distances.py
+++ fabrics/helpers/distances.py
@@ -1,8 +1,9 @@
 import casadi as ca
-import numpy as np
-from typing import List, Union
+from typing import List
 
-def closest_point_to_line(point: ca.SX, line_start: ca.SX, line_end: ca.SX) -> ca.SX:
+def closest_point_to_line(
+    point: ca.SX, line_start: ca.SX, line_end: ca.SX
+) -> ca.SX:
     line_vector = line_end - line_start
     point_vector = point - line_start
     t = ca.dot(point_vector, line_vector) / ca.dot(line_vector, line_vector)
@@ -11,8 +12,6 @@ def closest_point_to_line(point: ca.SX, line_start: ca.SX, line_end: ca.SX) -> c
 
 def clamp(a: ca.SX, a_min: float, a_max: float):
     return ca.fmin(a_max, ca.fmax(a, a_min))
-
-
 
 def point_to_line(point: ca.SX, line_start: ca.SX, line_end: ca.SX) -> ca.SX:
     line_vec = line_end - line_start
@@ -39,19 +38,23 @@ def line_to_line_sampled(
     line_1_end: ca.SX,
     line_2_start: ca.SX,
     line_2_end: ca.SX,
-    samples: int = 100) -> ca.SX:
+    samples: int = 100,
+) -> ca.SX:
     """
-    Computes the distance between two lines ignoring the case that they are 
+    Computes the distance between two lines ignoring the case that they are
     intersecting.
+
     """
     distance = ca.fmin(
-        point_to_line(line_1_start, line_2_start, line_2_end), 
-        point_to_line(line_1_end, line_2_start, line_2_end), 
+        point_to_line(line_1_start, line_2_start, line_2_end),
+        point_to_line(line_1_end, line_2_start, line_2_end),
     )
     for i in range(samples):
-        eta = 1/samples * i
-        line_point = (1-eta) * line_1_start + eta * line_1_end
-        distance = ca.fmin(distance, point_to_line(line_point, line_2_start, line_2_end))
+        eta = 1 / samples * i
+        line_point = (1 - eta) * line_1_start + eta * line_1_end
+        distance = ca.fmin(
+            distance, point_to_line(line_point, line_2_start, line_2_end)
+        )
 
     return distance
 
@@ -59,9 +62,10 @@ def line_to_line(
     line_1_start: ca.SX,
     line_1_end: ca.SX,
     line_2_start: ca.SX,
-    line_2_end: ca.SX) -> ca.SX:
+    line_2_end: ca.SX,
+) -> ca.SX:
     """
-    Computes the distance between two lines according to 
+    Computes the distance between two lines according to
     Real-Time Collision Detection by Christer Ericson, page 148
     """
     eps = 1e-5
@@ -73,49 +77,37 @@ def line_to_line(
     f = ca.dot(d2, r)
     c = ca.dot(d1, r)
     b = ca.dot(d1, d2)
-    denom = a*e-b*b
+    denom = a * e - b * b
     s = ca.if_else(
         a <= eps,
         0.0,
         ca.if_else(
             e <= eps,
-            clamp(-c/a, 0.0, f),
+            clamp(-c / a, 0.0, 1.0),
             ca.if_else(
-                denom != 0.0,
-                clamp((b*f- c*e)/denom, 0.0, f),
-                0.0
-            )
-        )
+                denom != 0.0, clamp((b * f - c * e) / denom, 0.0, 1.0), 0.0
+            ),
+        ),
     )
 
     t = ca.if_else(
         a <= eps,
-        clamp(f / e, 0.0, f),
-        ca.if_else(
-            e <= eps,
-            0.0,
-            (b*s+f)/e
-        )
+        clamp(f / e, 0.0, 1.0),
+        ca.if_else(e <= eps, 0.0, (b * s + f) / e),
     )
-    s_1 = ca.if_else(
-        t < 0.0,
-        clamp(-c / a, 0.0, f),
-        s
-    )
-    s_2 = ca.if_else(
-        t > f,
-        clamp((b*f-c*e)/denom, 0.0, f),
-        s_1
-    )
+    s_1 = ca.if_else(t < 0.0, clamp(-c / a, 0.0, 1.0), s)
+    s_2 = ca.if_else(t > f, clamp((b * f - c * e) / denom, 0.0, 1.0), s_1)
     t_1 = clamp(t, 0.0, f)
     c1 = line_1_start + d1 * s_2
     c2 = line_2_start + d2 * t_1
     distance = ca.if_else(
-        ca.logic_and(a <= eps, e<=eps),
-        ca.sqrt(ca.dot(line_1_start - line_2_start, line_1_start - line_2_start)),
-        ca.dot(c1-c2, c1-c2)
+        ca.logic_and(a <= eps, e <= eps),
+        ca.dot(line_1_start - line_2_start, line_1_start - line_2_start),
+        ca.dot(c1 - c2, c1 - c2),
     )
     return ca.sqrt(distance)
+    # ca.vertcat(ca.sqrt(distance), t_1, s_2, c1, c2)
+
 
 def point_to_plane(point: ca.SX, plane: ca.SX) -> ca.SX:
     distance = ca.fabs(ca.dot(plane[0:3], point) + plane[3]) / ca.norm_2(
@@ -141,12 +133,17 @@ def line_to_plane(line_start: ca.SX, line_end: ca.SX, plane: ca.SX):
     distance = ca.if_else(product_dot_products < 0, 0.0, min_distance_ends)
     return distance
 
+
 def capsule_to_plane(
     capsule_centers: List[ca.SX],
     plane: ca.SX,
     capsule_radius: ca.SX,
 ) -> ca.SX:
-    return line_to_plane(capsule_centers[0], capsule_centers[1], plane) - capsule_radius
+    return (
+        line_to_plane(capsule_centers[0], capsule_centers[1], plane)
+        - capsule_radius
+    )
+
 
 def capsule_to_capsule(
     capsule_1_centers: List[ca.SX],
@@ -154,10 +151,16 @@ def capsule_to_capsule(
     capsule_1_radius: ca.SX,
     capsule_2_radius: ca.SX,
 ) -> ca.SX:
-    return line_to_line(
-        capsule_1_centers[0], capsule_1_centers[1],
-        capsule_2_centers[0], capsule_2_centers[1]
-    ) - capsule_1_radius - capsule_2_radius
+    return (
+        line_to_line(
+            capsule_1_centers[0],
+            capsule_1_centers[1],
+            capsule_2_centers[0],
+            capsule_2_centers[1],
+        )
+        - capsule_1_radius
+        - capsule_2_radius
+    )
 
 
 def capsule_to_sphere(
@@ -174,86 +177,132 @@ def capsule_to_sphere(
 
 
 def cuboid_to_point_half_distances(
-        rectangle_center: ca.SX,
-        rectangle_size: ca.SX,
-        point: ca.SX,
-    ) -> List[ca.SX]:
+    rectangle_center: ca.SX,
+    rectangle_size: ca.SX,
+    point: ca.SX,
+) -> List[ca.SX]:
     half_distances = []
     for i in range(point.size()[0]):
         half_distances.append(
-            ca.fmax(ca.fabs(point[i] - rectangle_center[i]) - rectangle_size[i]/2, 0.0))
+            ca.fmax(
+                ca.fabs(point[i] - rectangle_center[i]) - rectangle_size[i] / 2,
+                0.0,
+            )
+        )
     return half_distances
 
+
 def rectangle_to_point(
-        rectangle_center: ca.SX,
-        rectangle_size: ca.SX,
-        point: ca.SX,
-    ) -> ca.SX:
+    rectangle_center: ca.SX,
+    rectangle_size: ca.SX,
+    point: ca.SX,
+) -> ca.SX:
     half_distances = cuboid_to_point_half_distances(
-        rectangle_center,
-        rectangle_size,
-        point
+        rectangle_center, rectangle_size, point
     )
     return ca.sqrt(half_distances[0] ** 2 + half_distances[1] ** 2)
 
+
 def rectangle_to_line(
-        rectangle_center: ca.SX,
-        rectangle_size: ca.SX,
-        line_start: ca.SX,
-        line_end: ca.SX,
-    ) -> ca.SX:
+    rectangle_center: ca.SX,
+    rectangle_size: ca.SX,
+    line_start: ca.SX,
+    line_end: ca.SX,
+) -> ca.SX:
     min_distance = ca.fmin(
-        rectangle_to_point(rectangle_center, rectangle_size, line_start), 
-        rectangle_to_point(rectangle_center, rectangle_size, line_end), 
+        rectangle_to_point(rectangle_center, rectangle_size, line_start),
+        rectangle_to_point(rectangle_center, rectangle_size, line_end),
     )
     for i in [-1, 1]:
         for j in [-1, 1]:
             index = [i, j]
             corner_transform = rectangle_size / 2 * index
             corner = rectangle_center + corner_transform
-            min_distance = ca.fmin(min_distance, point_to_line(corner, line_start, line_end))
+            min_distance = ca.fmin(
+                min_distance, point_to_line(corner, line_start, line_end)
+            )
     return min_distance
 
+
 def cuboid_to_point(
-        cuboid_center: ca.SX,
-        cuboid_size: ca.SX,
-        point: ca.SX,
-    ) -> ca.SX:
+    cuboid_center: ca.SX,
+    cuboid_size: ca.SX,
+    point: ca.SX,
+) -> ca.SX:
     half_distances = cuboid_to_point_half_distances(
-        cuboid_center,
-        cuboid_size,
-        point
+        cuboid_center, cuboid_size, point
     )
     return ca.sqrt(
         half_distances[0] ** 2 + half_distances[1] ** 2 + half_distances[2] ** 2
     )
 
+
+def edge_of_cuboid(
+    cuboid_center: ca.SX, cuboid_size: ca.SX, index: int
+) -> ca.SX:
+    edges = [
+        [[-1, -1, -1], [1, -1, -1]],
+        [[-1, -1, -1], [-1, 1, -1]],
+        [[-1, -1, -1], [-1, -1, 1]],
+        [[-1, -1, 1], [1, -1, 1]],
+        [[-1, -1, 1], [-1, 1, 1]],
+        [[-1, 1, -1], [1, 1, -1]],
+        [[-1, 1, -1], [-1, 1, 1]],
+        [[1, -1, -1], [1, 1, -1]],
+        [[1, -1, -1], [1, -1, 1]],
+        [[1, 1, 1], [-1, 1, 1]],
+        [[1, 1, 1], [1, -1, 1]],
+        [[1, 1, 1], [1, 1, -1]],
+    ]
+    edge_start: ca.SX = cuboid_center + cuboid_size / 2 * edges[index][0]
+    edge_end: ca.SX = cuboid_center + cuboid_size / 2 * edges[index][1]
+    return ca.vertcat(edge_start, edge_end)
+
+
 def cuboid_to_line(
-        cuboid_center: ca.SX,
-        cuboid_size: ca.SX,
-        line_start: ca.SX,
-        line_end: ca.SX,
-        samples: int = 100,
-    ) -> ca.SX:
-    distance = ca.SX(ca.inf)
-    for i in range(samples+1):
-        eta = 1/samples * i
-        line_point = (1-eta) * line_start + eta * line_end
-        distance = ca.fmin(distance, cuboid_to_point(cuboid_center, cuboid_size, line_point))
+    cuboid_center: ca.SX,
+    cuboid_size: ca.SX,
+    line_start: ca.SX,
+    line_end: ca.SX,
+) -> ca.SX:
+    distance = ca.fmin(
+        cuboid_to_point(cuboid_center, cuboid_size, line_start),
+        cuboid_to_point(cuboid_center, cuboid_size, line_end),
+    )
+    for i in range(12):
+        edge = edge_of_cuboid(cuboid_center, cuboid_size, i)
+        edge_start = edge[0:3]
+        edge_end = edge[3:6]
+        distance_line_edge = line_to_line(
+            edge_start, edge_end, line_start, line_end
+        )
+        distance = ca.fmin(distance, distance_line_edge)
     return distance
 
+
 def cuboid_to_sphere(
-        cuboid_center: ca.SX,
-        sphere_center: ca.SX,
-        cuboid_size: ca.SX,
-        sphere_size: ca.SX,
-    ) -> ca.SX:
-    return ca.fmax(0.0, cuboid_to_point(cuboid_center, cuboid_size, sphere_center) - sphere_size)
+    cuboid_center: ca.SX,
+    sphere_center: ca.SX,
+    cuboid_size: ca.SX,
+    sphere_size: ca.SX,
+) -> ca.SX:
+    return ca.fmax(
+        0.0,
+        cuboid_to_point(cuboid_center, cuboid_size, sphere_center)
+        - sphere_size,
+    )
+
 
 def cuboid_to_capsule(
-        cuboid_center: ca.SX,
-        capsule_centers: List[ca.SX],
-        cuboid_size: ca.SX,
-        capsule_radius: ca.SX,
-    ) -> ca.SX:
-    return ca.fmax(cuboid_to_line(cuboid_center, cuboid_size, capsule_centers[0], capsule_centers[1]) - capsule_radius, 0.0)
+    cuboid_center: ca.SX,
+    capsule_centers: List[ca.SX],
+    cuboid_size: ca.SX,
+    capsule_radius: ca.SX,
+) -> ca.SX:
+    return ca.fmax(
+        cuboid_to_line(
+            cuboid_center, cuboid_size, capsule_centers[0], capsule_centers[1]
+        )
+        - capsule_radius,
+        0.0,
+    )
--- tests/unittests/helpers/test_distances.py
+++ tests/unittests/helpers/test_distances.py
@@ -1,14 +1,7 @@
 import casadi as ca
 import pytest
 import numpy as np
-from fabrics.helpers.distances import (
-    closest_point_to_line,
-    line_to_line,
-    point_to_line,
-    point_to_plane,
-    line_to_plane,
-    capsule_to_sphere,
-)
+from fabrics.helpers.distances import *
 
 def test_closest_point_line():
     line_start = ca.SX.sym("line_start", 3)
@@ -88,6 +81,63 @@ def test_distance_line_line():
         )
     )
     assert distance_numpy == 1.2
+
+    line_1_start_numpy = np.array([1, -0.2, 0])
+    line_1_end_numpy = np.array([-1, -0.2, 0])
+    line_2_start_numpy = np.array([2, 1, 0])
+    line_2_end_numpy = np.array([-2, 1, 0])
+    distance_numpy = np.array(
+        distance_function(
+            line_1_start_numpy,
+            line_1_end_numpy,
+            line_2_start_numpy,
+            line_2_end_numpy,
+        )
+    )
+    assert distance_numpy == 1.2
+
+    line_1_start_numpy = np.array([0, 0, 0])
+    line_1_end_numpy = np.array([0, 1, 0])
+    line_2_start_numpy = np.array([2, 2, 0])
+    line_2_end_numpy = np.array([-2, 2, 0])
+    distance_numpy = np.array(
+        distance_function(
+            line_1_start_numpy,
+            line_1_end_numpy,
+            line_2_start_numpy,
+            line_2_end_numpy,
+        )
+    )
+    assert distance_numpy == 1.0
+    edges = [
+        [[0, 0, 0], [1, 0, 0]],
+        [[0, 0, 0], [0, 1, 0]],
+        [[0, 0, 0], [0, 0, 1]],
+        [[0, 0, 1], [1, 0, 1]],
+        [[0, 0, 1], [0, 1, 1]],
+        [[0, 1, 0], [1, 1, 0]],
+        [[0, 1, 0], [0, 1, 1]],
+        [[1, 0, 0], [1, 1, 0]],
+        [[1, 0, 0], [1, 0, 1]],
+        [[1, 1, 1], [0, 1, 1]],
+        [[1, 1, 1], [1, 0, 1]],
+        [[1, 1, 1], [1, 1, 0]],
+    ]
+    for edge in edges:
+        line_1_start_numpy = np.array(edge[0])
+        line_1_end_numpy = np.array(edge[1])
+        line_2_start_numpy = np.array([-2, 3, 0.5])
+        line_2_end_numpy = np.array([2, 3, 0.5])
+        distance_numpy = np.array(
+            distance_function(
+                line_1_start_numpy,
+                line_1_end_numpy,
+                line_2_start_numpy,
+                line_2_end_numpy,
+            )
+        )
+        assert distance_numpy >= 2.0
+
 
 
 
@@ -176,3 +226,256 @@ def test_distance_capsule_sphere():
         )
     )
     assert distance_numpy == np.sqrt(2) - 0.3 - 0.4
+
+def test_edges_of_cuboid():
+    cuboid_center = ca.SX.sym("cuboid_center", 3)
+    cuboid_size = ca.SX.sym("cuboid_size", 3)
+    cuboid_center_numpy = np.array([1.0, 0.0, 0.3])
+    cuboid_size_numpy = np.array([1.0, 0.5, 1.0])
+    function_arguments = [cuboid_center, cuboid_size]
+
+    edge_expression = edge_of_cuboid(cuboid_center, cuboid_size, 0)
+    edge_function = ca.Function('edge_function', function_arguments, [edge_expression])
+
+    edge_numpy = np.array(edge_function(cuboid_center_numpy, cuboid_size_numpy))[:, 0]
+    assert np.array_equal(edge_numpy[0:3], np.array([0.5, -0.25, -0.2]))
+    assert np.array_equal(edge_numpy[3:6], np.array([1.5, -0.25, -0.2]))
+
+    edge_expression = edge_of_cuboid(cuboid_center, cuboid_size, 1)
+    edge_function = ca.Function('edge_function', function_arguments, [edge_expression])
+
+    edge_numpy = np.array(edge_function(cuboid_center_numpy, cuboid_size_numpy))[:, 0]
+    assert np.array_equal(edge_numpy[0:3], np.array([0.5, -0.25, -0.2]))
+    assert np.array_equal(edge_numpy[3:6], np.array([0.5, 0.25, -0.2]))
+
+
+def test_distance_rectangle_point():
+    rectangle_center = ca.SX.sym("rectangle_center", 2)
+    rectangle_size = ca.SX.sym("rectangle_size", 2)
+    point = ca.SX.sym("point", 2)
+    distance_expression = rectangle_to_point(rectangle_center, rectangle_size, point)
+    function_arguments = [rectangle_center, rectangle_size, point]
+    distance_function = ca.Function(
+        "distance_function", function_arguments, [distance_expression]
+    )
+
+    rectangle_center_numpy = np.array([0.0, 0.0])
+    rectangle_size_numpy = np.array([1.0, 1.0])
+    point_numpy = np.array([3.0, 0.0])
+    distance_numpy = np.array(
+        distance_function(rectangle_center_numpy, rectangle_size_numpy, point_numpy)
+    )
+    assert distance_numpy == 2.5
+    point_numpy = np.array([3.0, 2.0])
+    distance_numpy = np.array(
+        distance_function(rectangle_center_numpy, rectangle_size_numpy, point_numpy)
+    )
+    assert distance_numpy == (2.5**2+1.5**2)**0.5
+
+def test_distance_rectangle_line():
+    rectangle_center = ca.SX.sym("rectangle_center", 2)
+    rectangle_size = ca.SX.sym("rectangle_size", 2)
+    line_start = ca.SX.sym("line_start", 2)
+    line_end = ca.SX.sym("line_end", 2)
+    distance_expression = rectangle_to_line(
+        rectangle_center,
+        rectangle_size,
+        line_start,
+        line_end
+    )
+    function_arguments = [rectangle_center, rectangle_size, line_start, line_end]
+    distance_function = ca.Function(
+        "distance_function", function_arguments, [distance_expression]
+    )
+
+    rectangle_center_numpy = np.array([0.0, 0.0])
+    rectangle_size_numpy = np.array([1.0, 1.0])
+    line_start_numpy = np.array([3.0, 4.0])
+    line_end_numpy = np.array([3.0, 0.0])
+    distance_numpy = np.array(
+        distance_function(
+            rectangle_center_numpy,
+            rectangle_size_numpy,
+            line_start_numpy,
+            line_end_numpy,
+        )
+    )
+    assert distance_numpy == pytest.approx(2.5)
+    line_start_numpy = np.array([1.5, 0.5])
+    line_end_numpy = np.array([-0.5, -1.5])
+    distance_numpy = np.array(
+        distance_function(
+            rectangle_center_numpy,
+            rectangle_size_numpy,
+            line_start_numpy,
+            line_end_numpy,
+        )
+    )
+    assert distance_numpy == pytest.approx(0.0)
+    line_start_numpy = np.array([1.5, 2.5])
+    line_end_numpy = np.array([1.5, -2.5])
+    distance_numpy = np.array(
+        distance_function(
+            rectangle_center_numpy,
+            rectangle_size_numpy,
+            line_start_numpy,
+            line_end_numpy,
+        )
+    )
+    assert distance_numpy == pytest.approx(1.0)
+
+def test_distance_cuboid_point():
+    rectangle_center = ca.SX.sym("rectangle_center", 3)
+    rectangle_size = ca.SX.sym("rectangle_size", 3)
+    point = ca.SX.sym("point", 3)
+    distance_expression = rectangle_to_point(rectangle_center, rectangle_size, point)
+    function_arguments = [rectangle_center, rectangle_size, point]
+    distance_function = ca.Function(
+        "distance_function", function_arguments, [distance_expression]
+    )
+    rectangle_center_numpy = np.array([0.0, 0.0, 0.0])
+    rectangle_size_numpy = np.array([1.0, 1.0, 0.0])
+
+    point_numpy = np.array([3.0, 0.0, 0.0])
+    distance_numpy = np.array(
+        distance_function(rectangle_center_numpy, rectangle_size_numpy, point_numpy)
+    )
+    assert distance_numpy == 2.5
+    rectangle_center_numpy = np.array([0.5, 0.5, 0.5])
+    rectangle_size_numpy = np.array([1.0, 1.0, 1.0])
+
+    point_numpy = np.array([1.5, 0.5, 0.0])
+    distance_numpy = np.array(
+        distance_function(rectangle_center_numpy, rectangle_size_numpy, point_numpy)
+    )
+    assert distance_numpy == 0.5
+    point_numpy = np.array([0.5, 0.5, 1.0])
+    distance_numpy = np.array(
+        distance_function(rectangle_center_numpy, rectangle_size_numpy, point_numpy)
+    )
+    assert distance_numpy == 0.0
+
+def test_distance_cuboid_sphere():
+    cuboid_center = ca.SX.sym("cuboid_center", 3)
+    cuboid_size = ca.SX.sym("cuboid_size", 3)
+    sphere_center = ca.SX.sym("sphere_center", 3)
+    sphere_size = ca.SX.sym("sphere_size", 1)
+    distance_expression = cuboid_to_sphere(
+        cuboid_center,
+        sphere_center,
+        cuboid_size,
+        sphere_size
+    )
+    function_arguments = [cuboid_center, sphere_center, cuboid_size, sphere_size]
+    distance_function = ca.Function(
+        "distance_function", function_arguments, [distance_expression]
+    )
+    cuboid_center_numpy = np.array([0.0, 0.0, 0.0])
+    cuboid_size_numpy = np.array([1.0, 1.0, 0.0])
+    sphere_center_numpy = np.array([3.0, 0.0, 0.0])
+    sphere_size_numpy = np.array([0.2])
+    distance_numpy = np.array(
+        distance_function(
+            cuboid_center_numpy,
+            sphere_center_numpy,
+            cuboid_size_numpy,
+            sphere_size_numpy)
+    )
+    assert distance_numpy == 2.3
+    cuboid_center_numpy = np.array([0.5, 0.5, 0.5])
+    cuboid_size_numpy = np.array([1.0, 1.0, 1.0])
+    sphere_size_numpy = np.array([0.2])
+
+    sphere_center_numpy = np.array([1.5, 0.5, 0.0])
+    distance_numpy = np.array(
+        distance_function(
+            cuboid_center_numpy,
+            sphere_center_numpy,
+            cuboid_size_numpy,
+            sphere_size_numpy)
+    )
+    assert distance_numpy == 0.3
+
+    sphere_center_numpy = np.array([0.5, 0.2, 1.0])
+    distance_numpy = np.array(
+        distance_function(
+            cuboid_center_numpy,
+            sphere_center_numpy,
+            cuboid_size_numpy,
+            sphere_size_numpy)
+    )
+    assert distance_numpy == 0.0
+
+def test_distance_cuboid_capsule():
+    cuboid_center = ca.SX.sym("cuboid_center", 3)
+    cuboid_size = ca.SX.sym("cuboid_size", 3)
+    capsule_centers = [
+        ca.SX.sym("capsule_center1", 3),
+        ca.SX.sym("capsule_center2", 3),
+    ]
+    capsule_radius = ca.SX.sym("capsule_radius", 1)
+    distance_expression = cuboid_to_capsule(
+        cuboid_center,
+        capsule_centers,
+        cuboid_size,
+        capsule_radius,
+    )
+    function_arguments = capsule_centers + [cuboid_center, cuboid_size, capsule_radius]
+    distance_function = ca.Function(
+        "distance_function", function_arguments, [distance_expression]
+    )
+    cuboid_center_numpy = np.array([0.5, 0.5, 0.5])
+    cuboid_size_numpy = np.array([1.0, 1.0, 1.0])
+    capsule_radius_numpy = np.array([0.2])
+
+    capsule_centers_numpy = [
+        np.array([-2.0, 3.0, 0.5]),
+        np.array([2.0, 3.0, 0.5])
+    ]
+    distance_numpy = np.array(
+        distance_function(
+            capsule_centers_numpy[0],
+            capsule_centers_numpy[1],
+            cuboid_center_numpy,
+            cuboid_size_numpy,
+            capsule_radius_numpy,
+        )
+    )
+    assert distance_numpy == 1.8
+    
+    cuboid_center_numpy = np.array([0.0, 0.0, 0.0])
+    cuboid_size_numpy = np.array([1.0, 1.0, 1.0])
+    capsule_radius_numpy = np.array([0.0])
+
+    capsule_centers_numpy = [
+        np.array([1.5, 0.5, 0.5]),
+        np.array([-0.5, -1.5, 0.5])
+    ]
+    distance_numpy = np.array(
+        distance_function(
+            capsule_centers_numpy[0],
+            capsule_centers_numpy[1],
+            cuboid_center_numpy,
+            cuboid_size_numpy,
+            capsule_radius_numpy,
+        )
+    )
+    assert distance_numpy == 0.0
+
+    cuboid_center_numpy = np.array([0.5, 0.5, 0.5])
+    cuboid_size_numpy = np.array([1.0, 1.0, 1.0])
+    capsule_radius_numpy = np.array([0.2])
+    capsule_centers_numpy = [
+        np.array([0.0, 3.0, 0.15]),
+        np.array([3.0, 0.0, 0.75])
+    ]
+    distance_numpy = np.array(
+        distance_function(
+            capsule_centers_numpy[0],
+            capsule_centers_numpy[1],
+            cuboid_center_numpy,
+            cuboid_size_numpy,
+            capsule_radius_numpy,
+        )
+    )
+    assert distance_numpy == pytest.approx(0.70710678-0.2)
