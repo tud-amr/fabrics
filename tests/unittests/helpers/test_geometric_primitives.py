f50ffab83a504662b3654d85c5670bbb3d9b45b2
Max Spahn <m.spahn@tudelft.nl>
2023-11-23 11:46:34 +0100

    ft[test]: Adds more test for geometric primitives.

--- tests/unittests/helpers/test_geometric_primitives.py
+++ tests/unittests/helpers/test_geometric_primitives.py
@@ -1,8 +1,10 @@
 import casadi as ca
 import numpy as np
 from fabrics.helpers.geometric_primitives import (
+    Cuboid,
     Sphere,
     Capsule,
+    Plane,
 )
 
 def test_sphere():
@@ -29,3 +31,38 @@ def test_capsule():
     assert capsule.radius == 0.2
     assert capsule.length == 0.5
     assert capsule.size == [0.2, 0.5]
+
+
+def test_cuboid():
+    cuboid = Cuboid('cuboid', sizes=[0.3, 0.4, 0.1])
+    assert cuboid.size[0] == 0.3
+    assert cuboid.size[1] == 0.4
+    assert cuboid.size[2] == 0.1
+    assert str(cuboid) == 'Cuboid: cuboid'
+
+def test_set_origin():
+    capsule = Capsule('capsule', radius=1, length=0.5)
+    origin = ca.SX(np.identity(4))
+    origin[0:3, 3] = np.array([0.3, 0.2, 0.1])
+    capsule.set_origin(origin)
+    assert capsule.position[0] == 0.3
+    centers = capsule.centers
+    center_fun = ca.Function('center_fun', [capsule.sym_length], centers)
+    centers_np = center_fun(np.array([0.5]))
+    assert centers_np[0][0] == 0.3
+    assert centers_np[0][1] == 0.2
+    assert centers_np[0][2] == 0.35
+    assert 'radius_capsule' in capsule.parameters
+
+def test_plane():
+    plane = Plane('plane', plane_equation = [0.0, 0.0, 1.0, 0.0])
+    assert plane.size == [0.0, 0.0, 1.0, 0.0]
+    assert plane.plane_equation == [0.0, 0.0, 1.0, 0.0]
+    assert 'plane' in plane.sym_size
+    assert isinstance(plane.sym_size, dict)
+    assert ca.is_equal(plane.sym_plane_equation, plane.sym_size['plane'])
+    assert 'plane' in plane.parameters
+
+
+
+
