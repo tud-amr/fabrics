c6a4a65abd7a4c9edfa415aa19f5e0b3a38ccf18
Max Spahn <m.spahn@tudelft.nl>
2023-12-07 16:52:02 +0100

    ft[mujoco]: Adds simple example with mujoco.

---
+++ examples/panda_mujoco.py
@@ -0,0 +1,205 @@
+import os
+from PIL import Image
+import gymnasium as gym
+import numpy as np
+
+from forwardkinematics.urdfFks.generic_urdf_fk import GenericURDFFk
+
+from urdfenvs.urdf_common.urdf_env import UrdfEnv
+from urdfenvs.robots.generic_urdf import GenericUrdfReacher
+from urdfenvs.sensors.full_sensor import FullSensor
+
+from urdfenvs.urdf_common.generic_mujoco_env import GenericMujocoEnv
+from mpscenes.goals.goal_composition import GoalComposition
+from mpscenes.obstacles.sphere_obstacle import SphereObstacle
+
+from fabrics.planner.parameterized_planner import ParameterizedFabricPlanner
+
+def initalize_environment(render=True):
+    """
+    Initializes the simulation environment.
+
+    Adds obstacles and goal visualizaion to the environment based and
+    steps the simulation once.
+    """
+    """
+    robots = [
+        GenericUrdfReacher(urdf="panda.urdf", mode="acc"),
+    ]
+    env: UrdfEnv  = gym.make(
+        "urdf-env-v0",
+        dt=0.01, robots=robots, render=render
+    )
+    """
+    env =  GenericMujocoEnv(xml_file='panda_scene.xml', render_mode="rgb_array")
+
+    full_sensor = FullSensor(
+            goal_mask=["position", "weight"],
+            obstacle_mask=['position', 'size'],
+            variance=0.0
+    )
+    # Definition of the obstacle.
+    static_obst_dict = {
+        "type": "sphere",
+        "geometry": {"position": [0.5, -0.3, 0.3], "radius": 0.1},
+    }
+    obst1 = SphereObstacle(name="staticObst", content_dict=static_obst_dict)
+    static_obst_dict = {
+        "type": "sphere",
+        "geometry": {"position": [-0.7, 0.0, 0.5], "radius": 0.1},
+    }
+    obst2 = SphereObstacle(name="staticObst", content_dict=static_obst_dict)
+    # Definition of the goal.
+    goal_dict = {
+        "subgoal0": {
+            "weight": 1.0,
+            "is_primary_goal": True,
+            "indices": [0, 1, 2],
+            "parent_link": "panda_link0",
+            "child_link": "panda_hand",
+            "desired_position": [0.1, -0.6, 0.4],
+            "epsilon": 0.05,
+            "type": "staticSubGoal",
+        },
+        "subgoal1": {
+            "weight": 5.0,
+            "is_primary_goal": False,
+            "indices": [0, 1, 2],
+            "parent_link": "panda_link7",
+            "child_link": "panda_hand",
+            "desired_position": [0.1, 0.0, 0.0],
+            "epsilon": 0.05,
+            "type": "staticSubGoal",
+        }
+    }
+    goal = GoalComposition(name="goal", content_dict=goal_dict)
+    obstacles = (obst1, obst2)
+    env.reset()
+    pos0=np.array([0,0,0,-1.57079,0,1.57079,-0.7853,0.04,0.04])
+    env.reset_model(pos=pos0)
+
+    """
+    env.add_sensor(full_sensor, [0])
+    for obst in obstacles:
+        env.add_obstacle(obst)
+    for sub_goal in goal.sub_goals():
+        env.add_goal(sub_goal)
+    env.set_spaces()
+    """
+    return (env, goal)
+
+
+def set_planner(goal: GoalComposition, degrees_of_freedom: int = 7):
+    """
+    Initializes the fabric planner for the panda robot.
+
+    This function defines the forward kinematics for collision avoidance,
+    and goal reaching. These components are fed into the fabrics planner.
+
+    In the top section of this function, an example for optional reconfiguration
+    can be found. Commented by default.
+
+    Params
+    ----------
+    goal: StaticSubGoal
+        The goal to the motion planning problem.
+    degrees_of_freedom: int
+        Degrees of freedom of the robot (default = 7)
+    """
+
+    ## Optional reconfiguration of the planner
+    # base_inertia = 0.03
+    # attractor_potential = "20 * ca.norm_2(x)**4"
+    # damper = {
+    #     "alpha_b": 0.5,
+    #     "alpha_eta": 0.5,
+    #     "alpha_shift": 0.5,
+    #     "beta_distant": 0.01,
+    #     "beta_close": 6.5,
+    #     "radius_shift": 0.1,
+    # }
+    # planner = ParameterizedFabricPlanner(
+    #     degrees_of_freedom,
+    #     forward_kinematics,
+    #     base_inertia=base_inertia,
+    #     attractor_potential=attractor_potential,
+    #     damper=damper,
+    # )
+    absolute_path = os.path.dirname(os.path.abspath(__file__))
+    with open(absolute_path + "/panda_for_fk.urdf", "r", encoding="utf-8") as file:
+        urdf = file.read()
+    forward_kinematics = GenericURDFFk(
+        urdf,
+        rootLink="panda_link0",
+        end_link="panda_link9",
+    )
+    planner = ParameterizedFabricPlanner(
+        degrees_of_freedom,
+        forward_kinematics,
+    )
+    collision_links = ['panda_link7', 'panda_link3', 'panda_link4']
+    collision_linsk = []
+    self_collision_pairs = {"panda_link7": ['panda_link3', 'panda_link4', 'panda_link2', 'panda_link1']}
+    self_collision_pairs = {}
+    panda_limits = [
+            [-2.8973, 2.8973],
+            [-1.7628, 1.7628],
+            [-2.8973, 2.8973],
+            [-3.0718, -0.0698],
+            [-2.8973, 2.8973],
+            [-0.0175, 3.7525],
+            [-2.8973, 2.8973]
+        ]
+    # The planner hides all the logic behind the function set_components.
+    planner.set_components(
+        collision_links=collision_links,
+        self_collision_pairs=self_collision_pairs,
+        goal=goal,
+        number_obstacles=0,
+        number_plane_constraints=0,
+        limits=panda_limits,
+    )
+    planner.concretize(mode="vel", time_step=0.01)
+    return planner
+
+
+def run_panda_example(n_steps=5000, render=True):
+    (env, goal) = initalize_environment(render)
+    planner = set_planner(goal)
+    # planner.export_as_c("planner.c")
+    action = np.zeros(8)
+    ob, *_ = env.step(action)
+    """
+    body_links={1: 0.1, 2: 0.1, 3: 0.1, 4: 0.1, 7: 0.1}
+    for body_link, radius in body_links.items():
+        env.add_collision_link(0, body_link, shape_type='sphere', size=[radius])
+    """
+
+
+    for i in range(n_steps):
+        q = ob[:7]
+        qdot = ob[8:-1]
+        arguments = dict(
+            q=q,
+            qdot=qdot,
+            x_goal_0=np.array([0.0, 0.3, 0.7]),
+            weight_goal_0=2,
+            x_goal_1=np.array([0.1, 0.0, 0.0]),
+            weight_goal_1=5,
+        )
+        action[0:7] = planner.compute_action(**arguments)
+        ob, reward, terminated, truncated, info = env.step(action)
+        rgb_array = env.render()
+        Image.fromarray(rgb_array).save("images/rgb_array_large_{:04d}.png".format(i))
+
+        """
+        if terminated or truncated:
+            print(info)
+            break
+        """
+    env.close()
+    return {}
+
+
+if __name__ == "__main__":
+    res = run_panda_example(n_steps=5000)
