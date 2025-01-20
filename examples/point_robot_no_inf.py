0325595517282236ef80a59335607eb08d473d1a
Max Spahn <max.spahn@de.abb.com>
2025-01-20 14:21:45 +0100

    Working on point robot that seems problematic with oscilotions.

--- examples/point_robot_no_inf.py
+++ examples/point_robot_no_inf.py
@@ -1,4 +1,5 @@
 import os
+import gymnasium as gym
 import numpy as np
 import time
 
@@ -13,15 +14,15 @@ from mpscenes.obstacles.sphere_obstacle import SphereObstacle
 from mpscenes.goals.goal_composition import GoalComposition
 
 from fabrics.planner.parameterized_planner import ParameterizedFabricPlanner
-import matplotlib.pyplot as plt
 
 # Fabrics example for a 3D point mass robot. The fabrics planner uses a 2D point
 # mass to compute actions for a simulated 3D point mass.
 #
 # todo: tune behavior.
+
 DT = 0.01
 
-def initalize_environment(render: bool, enforce_real_time: bool):
+def initalize_environment(render):
     """
     Initializes the simulation environment.
 
@@ -54,12 +55,12 @@ def initalize_environment(render: bool, enforce_real_time: bool):
     )
     static_obst_dict = {
             "type": "sphere",
-            "geometry": {"position": [2.0, 0.8, 0.0], "radius": 1.0},
+            "geometry": {"position": [2.0, 0.0, 0.0], "radius": 1.0},
     }
     obst1 = SphereObstacle(name="staticObst1", content_dict=static_obst_dict)
     goal_dict = {
             "subgoal0": {
-                "weight": 0.25,
+                "weight": 2.5,
                 "is_primary_goal": True,
                 "indices": [0, 1],
                 "parent_link" : 'world',
@@ -71,7 +72,7 @@ def initalize_environment(render: bool, enforce_real_time: bool):
     }
     augmented_goal_dict = {
             "subgoal0": {
-                "weight": 0.25,
+                "weight": 2.5,
                 "is_primary_goal": True,
                 "indices": [0, 1, 2],
                 "parent_link" : 'world',
@@ -92,14 +93,12 @@ def initalize_environment(render: bool, enforce_real_time: bool):
         goals=goal_list,
         sensors=sensors,
         render=render,
-        enforce_real_time=enforce_real_time,
-        n_sub_steps=1,
-        dt = DT,
+        enforce_real_time=True,
     ).unwrapped
 
     # Set the initial position and velocity of the point mass.
     pos0 = np.array([-2.0, 0.5, 0.0])
-    vel0 = np.array([1.0, 0.0, 0.0])
+    vel0 = np.array([0.1, 0.0, 0.0])
     # Definition of the obstacle.
     # Definition of the goal.
     env.reset(pos=pos0, vel=vel0)
@@ -130,35 +129,16 @@ def set_planner(goal: GoalComposition):
         root_link="world",
         end_links="base_link",
     )
-    '''
-    collision_geometry = "-2.0 / (x ** 1) * xdot ** 2"
-    collision_finsler = "1.0/(x**2) * (1 - ca.heaviside(xdot))* xdot**2"
-    collision_finsler = "1.0/(x**2) * xdot**2"
+    # collision_geometry = "-2.0 / (x ** 1) * xdot ** 2"
+    collision_geometry = "-2*ca.log(1 + ca.exp(-100*x+50)) * xdot ** 2"
+    # collision_finsler = "1.0/(x**2) * (1 - ca.heaviside(xdot))* xdot**2"
+    # collision_finsler = "2*ca.log(1 + ca.exp(-10*x+50)) * xdot ** 2"
+    collision_finsler = "2*ca.log(1 + ca.exp(-100*x+50)) * xdot ** 2"
     planner = ParameterizedFabricPlanner(
         degrees_of_freedom,
         forward_kinematics,
         collision_geometry=collision_geometry,
-        collision_finsler=collision_finsler,
-        forcing_type = "simply_damped",
-        damper_beta = "0.00",
-    )
-    '''
-    collision_geometry = "-100/(1+ca.exp(4 * x - 3)) * (1 - ca.heaviside(xdot)) * xdot ** 2"
-    collision_geometry = "-100/(1+ca.exp(4 * x - 3)) * xdot ** 2"
-    collision_finsler = "100/(1+ca.exp(4 * x - 3)) * (1 - ca.heaviside(xdot)) * xdot ** 2"
-    #collision_finsler = "100/(1+ca.exp(2 * x - 3)) * xdot ** 2"
-
-    attractor_potential: str = "1.0 * ca.dot(x, ca.mtimes(np.identity(2), x))"
-    attractor_metric: str = "ca.SX(np.identity(x.size()[0]))"
-    planner = ParameterizedFabricPlanner(
-        degrees_of_freedom,
-        forward_kinematics,
-        collision_geometry=collision_geometry,
-        collision_finsler=collision_finsler,
-        attractor_potential=attractor_potential,
-        attractor_metric=attractor_metric,
-        forcing_type = "simply_damped",
-        damper_beta = "5.0",
+        collision_finsler=collision_finsler
     )
     collision_links = ["base_link"]
     # The planner hides all the logic behind the function set_components.
@@ -171,7 +151,7 @@ def set_planner(goal: GoalComposition):
     return planner
 
 
-def run_point_robot_urdf(n_steps=10000, render=True, enforce_real_time=True):
+def run_point_robot_urdf(n_steps=10000, render=False, enforce_real_time=True):
     """
     Set the gym environment, the planner and run point robot example.
     The initial zero action step is needed to initialize the sensor in the
@@ -184,46 +164,39 @@ def run_point_robot_urdf(n_steps=10000, render=True, enforce_real_time=True):
     render
         Boolean toggle to set rendering on (True) or off (False).
     """
-    (env, goal) = initalize_environment(render, enforce_real_time)
+    (env, goal) = initalize_environment(render)
     planner = set_planner(goal)
 
     action = np.array([0.0, 0.0, 0.0])
     ob, *_ = env.step(action)
 
-    vels = []
-    times = []
-
     for _ in range(n_steps):
-        t0 = time.time()
+        t0 = time.perf_counter()
         # Calculate action with the fabric planner, slice the states to drop Z-axis [3] information.
         ob_robot = ob['robot_0']
-        qdot=ob_robot["joint_state"]["velocity"]
-        vel = np.linalg.norm(qdot)
-        vels.append(vel)
-        times.append(env.t)
+        pos_obs = ob_robot['FullSensor']['obstacles'][0]['position'] #[-1.5, 0.5, 0.] #
         action = planner.compute_action(
             q=ob_robot["joint_state"]["position"],
             qdot=ob_robot["joint_state"]["velocity"],
             x_goal_0=ob_robot['FullSensor']['goals'][0]['position'][0:2],
             weight_goal_0=ob_robot['FullSensor']['goals'][0]['weight'],
-            x_obst_0=ob_robot['FullSensor']['obstacles'][0]['position'],
-            radius_obst_0=ob_robot['FullSensor']['obstacles'][0]['size'],
+            x_obst_0=pos_obs,
+            radius_obst_0=1., #ob_robot['FullSensor']['obstacles'][0]['size'],
             radius_body_base_link=np.array([0.2])
         )
-        t1 = time.time()
-        ob, _, done, _, info = env.step(action)
-        if done or vel < 1e-3:
-            print(info)
-            break
-        t2 = time.time()
+        t1 = time.perf_counter()
+        ob, *_, = env.step(action)
+        print("action: ", action)
+        t2 = time.perf_counter()
         #print(f"Time step was actually {t2-t0}")
         #print(f"Time for compute was {t1-t0}")
-        #print(f"Time for render was {t2-t0}")w
-
-    plt.plot(times, vels)
-    plt.show()
+        distance = np.linalg.norm(ob_robot["joint_state"]["position"][0:2] - pos_obs[0:2]) - 1.0 - 0.2
+        print("distance=", distance)
+        if distance < 0.:
+            print("collision occured!!")
+        # print(f"Time for render was {t2-t1}")
     env.close()
     return {}
 
 if __name__ == "__main__":
-    res = run_point_robot_urdf(n_steps=int(1e5), render=False, enforce_real_time=False)
+    res = run_point_robot_urdf(n_steps=2000, render=True)
